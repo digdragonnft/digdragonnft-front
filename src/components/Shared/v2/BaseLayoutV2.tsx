1f72ece76d16ef8eadfad71590c5e29b17a6bc67
mojisejr <nonthasak.l@gmail.com>
2024-01-12 21:58:21 +0700

    upd: metadata

--- src/components/Shared/v2/BaseLayoutV2.tsx
+++ src/components/Shared/v2/BaseLayoutV2.tsx
@@ -1,8 +1,16 @@
+import Head from "next/head";
 import { ReactNode } from "react";
 
 export default function BaseLayoutV2({ children }: { children: ReactNode }) {
   return (
     <div className="relative z-[1] min-h-screen w-full overflow-auto bg-gradient-to-br from-black from-[40%] via-slate-900 via-[50%] to-black to-[60%]">
+      <Head>
+        <title>DigDragon NFT Official</title>
+        <meta
+          name="description"
+          content="the group of people who admired NFT technology and Crypto mining. Therefore the holders can join many of our activities"
+        />
+      </Head>
       <img
         className="absolute bottom-0 right-0 z-[-1]"
         src="/images/hero1.png"
